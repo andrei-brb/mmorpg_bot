597fbffa9d54019dced208fe30c1a44cc245f5d2
andrei-brb <andreisebastian0@hotmail.com>
2026-02-27 22:43:03 +0100

    Move box inventory to InventoryCog, remove duplicate visual_inventory_cog

--- main.py
+++ main.py
@@ -109,7 +109,6 @@ class MMORPGBot(commands.Bot):
             "cogs.combat.combat_cog",
             "cogs.exploration.exploration_cog",
             "cogs.inventory.inventory_cog",
-            "cogs.inventory.visual_inventory_cog",
             "cogs.guild.guild_cog",
             "cogs.economy.economy_cog",
             "cogs.events.events_cog",
